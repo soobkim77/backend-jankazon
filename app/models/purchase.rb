47a497935f2b7ccdab9fabfd6aedaecaee26c948
Soo-Beom <soobeomkim77@gmail.com>
2021-06-10 18:58:49 -0700

    added model relationships

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,4 @@
 class Purchase < ApplicationRecord
   belongs_to :item
-  belongs_to :buyer
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :purchases, :dependent => :destroy
+    has_many :items, through: :purchases, :dependent => :destroy
+    validates  :username, uniqueness: { case_sensitive: false}
 end
